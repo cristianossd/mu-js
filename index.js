cb3f4ddc1d776fa4165e943534fac1475fc8d9f7
Cristiano Santos <cristianossd@gmail.com>
2014-12-20 12:29:52 -0300

    Simulate a battle between two characters

--- index.js
+++ index.js
@@ -9,14 +9,41 @@ var generateAttributes = function (){
 	return character;
 }
 
-var calculateDamage = function (charOne,charTwo){
+var calculateDamage = function (charOne, charTwo){
 	var totalDamage = charOne.strength * Math.floor(Math.random()*6)+1 - charTwo.armor * Math.floor(Math.random()*6)+1;
-	console.log(totalDamage);
+    return totalDamage;
 }
 
 var simulateBattle = function (){
-	var charOne = generateAttributes(), charTwo = generateAttributes();
+	var charOne = generateAttributes(), charTwo = generateAttributes(), turn = 0;
 	console.log(charOne);
 	console.log(charTwo);
-	calculateDamage(charOne,charTwo);
+	
+    
+    while(charOne.health > 0 && charTwo.health > 0) {
+        if (turn == 0) {
+            damage = calculateDamage(charOne, charTwo);
+            if (damage > 0) {
+                console.log("charOne damaged " + damage);
+                charTwo.health -= damage;
+            }
+            else
+                console.log("charOne didn't damage");
+        }
+        else {
+            damage = calculateDamage(charTwo, charOne);
+            if (damage > 0) {
+                console.log("charTwo damaged " + damage);
+                charOne.health -= damage;
+            }
+            else
+                console.log("charTwo didn't damage");
+        }
+        turn = 1 - turn;
+    }
+    
+    if (charTwo.health < 0)
+        console.log("* charOne");
+    else
+        console.log("* charTwo");
 }
