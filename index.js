21449903013fab2df6c234bbe36048ed5c482faf
paulo cardoso <paulo16061@gmail.com>
2014-12-20 11:07:04 -0300

    Generate characters and calculate damage

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var simulateBattle = function (attack, armor){
+var generateAttributes = function (){
+	var character = {
+		strength: Math.floor(Math.random()*6)+1,
+		armor: Math.floor(Math.random()*6)+1,
+		resistance: Math.floor(Math.random()*6)+1,
+		health: 0
+	};
+	character.health = character.resistance*5;
+	return character;
+}
 
-	var output = (attack.value > armor.value) ? "First" : "Second";
-	console.log(output); 
+var calculateDamage = function (charOne,charTwo){
+	var totalDamage = charOne.strength * Math.floor(Math.random()*6)+1 - charTwo.armor * Math.floor(Math.random()*6)+1;
+	console.log(totalDamage);
+}
+
+var simulateBattle = function (){
+	var charOne = generateAttributes(), charTwo = generateAttributes();
+	console.log(charOne);
+	console.log(charTwo);
+	calculateDamage(charOne,charTwo);
 }
