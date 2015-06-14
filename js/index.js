a1446e026451205a1c1c48cd73e2cde0e49a0a9a
cardoso222 <paulo16061@gmail.com>
2015-06-14 10:11:09 -0300

    Adiciona construção de personagens

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 
+function character() {
+
+	var strength;
+	var armor;
+	var skill;
+	var firePower;
+	var health;
+}
+
+function randomize() {
+
+	return Math.floor(Math.random() * 10);
+}
+
+function generateCharacter() {
+
+	var newCharacter = new character();
+	
+	newCharacter.strength = randomize();
+	newCharacter.armor = randomize();
+	newCharacter.skill = randomize();
+	newCharacter.firePower = randomize();
+	newCharacter.health = (newCharacter.skill * 5) + 1;
+
+	return console.log(newCharacter); 
+}
